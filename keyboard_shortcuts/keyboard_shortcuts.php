5691c758adfe04574fb9bfb1d01a618367f8bf75
Andrew Manning <tamanning@zoho.com>
2016-04-27 21:51:58 -0400

    keyboard shortcuts work in 8e8df26066d806c663e3eecc7d869d611e311f81

--- keyboard_shortcuts/keyboard_shortcuts.php
+++ keyboard_shortcuts/keyboard_shortcuts.php
@@ -12,18 +12,15 @@
 
 function keyboard_shortcuts_load() {
 	register_hook('page_end', 'addon/keyboard_shortcuts/keyboard_shortcuts.php', 'keyboard_shortcuts_script');
-
 }
 
 function keyboard_shortcuts_unload() {
-    unregister_hook('page_end', 'addon/keyboard_shortcuts/keyboard_shortcuts.php', 'keyboard_shortcuts_script');
+	unregister_hook('page_end', 'addon/keyboard_shortcuts/keyboard_shortcuts.php', 'keyboard_shortcuts_script');
 }
 
 function keyboard_shortcuts_install() {}
 function keyboard_shortcuts_uninstall() {}
 
-function keyboard_shortcuts_script() {
-	//head_add_js('addon/keyboard_shortcuts/keyboard_shortcuts.js');
-	$content = '<script type="text/javascript" src="' . z_root() . '/addon/keyboard_shortcuts/keyboard_shortcuts.js"></script>' . "\r\n";
-	return $content;
+function keyboard_shortcuts_script(&$footer) {
+	head_add_js('addon/keyboard_shortcuts/keyboard_shortcuts.js');
 }
