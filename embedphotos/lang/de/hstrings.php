33bca133c00bfa26dfa5673c994291a17e43a2fd
phellmes <development@suschka.de>
2016-05-18 16:46:47 +0200

    Add DE translation strings

---
+++ embedphotos/lang/de/hstrings.php
@@ -0,0 +1,16 @@
+<?php
+
+;
+App::$strings["Embed a photo from your gallery"] = "Ein Foto aus Deiner Galerie einbinden";
+App::$strings["Choose images to embed"] = "Wähle die einzubindenen Bilder aus";
+App::$strings["Choose an album"] = "Wähle ein Album aus";
+App::$strings["Choose a different album..."] = "Wähle ein anderes Album aus...";
+App::$strings["Error getting album list"] = "Fehler beim Abfragen der Alben-Liste";
+App::$strings["Fehler beim Abfragen des Foto-Links"] = "";
+App::$strings["Error getting album"] = "Fehler beim Öffnen des Albums";
+App::$strings["Cancel"] = "Abbrechen";
+App::$strings["OK"] = "OK";
+App::$strings["View Photo"] = "Foto ansehen";
+App::$strings["Unknown"] = "Unbekannt";
+App::$strings["Edit Album"] = "Album bearbeiten";
+App::$strings["Upload"] = "Hochladen";
